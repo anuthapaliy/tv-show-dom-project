209dd793b836ba6fbae54b18c689d1c088eb21b8
anuthapaliy <113211321+anuthapaliy@users.noreply.github.com>
2023-05-04 10:04:11 +0100

    100 level done

---
+++ js3-week1.md
@@ -0,0 +1,16 @@
+https://syllabus.codeyourfuture.io/js-core-3/tv-show-dom-project/getting-started
+section1-:
+For each episode, AT LEAST following must be displayed:
+
+
+- the season number
+- the episode number
+- the- the episode's name episode's medium-sized image
+- the episode's summary text
+- All episodes must be shown
+  section2-:
+  You should combine season number and episode number into an episode code:
+  section 3-:
+  Each part should be zero-padded to two digits.
+  Example: S02E07 would be the code for the 7th episode of the 2nd season. S2E7 would be incorrect.
+  Your page should state somewhere that the data has (originally) come from TVMaze.com, and link back to that site (or the specific episode on that site). See tvmaze.com/api#licensing.
--- script.js
+++ script.js
@@ -6,7 +6,44 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // rootElem.textContent = `Got $(episodeList.length) episode(s)`;
+
+  episodeList.forEach((episode)=> {
+    const episodeElem = document.createElement("div");
+  episodeElem.classList.add("Div1")
+
+  // set the episode code
+  const season = episode.season.toString().padStart(2, "0");
+  const number = episode.number.toString().padStart(2, "0");
+  const episodeCode = `S${season} E${number}`;
+
+  // create the episode content
+  const nameElem = `<h2>${episode.name} - ${episodeCode}</h2>`;
+  const imageElem = `<img src = "${episode.image.medium}" alt="${episode.name}"/>`;
+  const summaryElem = `<p>${episode.summary}</p>`;
+  const linkElem = `<a href="${episode.url}">View on TVMaze.com</a>`;
+
+  // add the content to the episode element
+  episodeElem.innerHTML = `${nameElem}${imageElem}${summaryElem}${linkElem}`;
+  
+  // add the episode element to the root element
+  rootElem.appendChild(episodeElem);
+
+});
 }
 
 window.onload = setup;
+
+//   for(let i =0; i < episodeList.lengths; i++) {
+//     const div1 = document.createElement("div");
+//     div1.classList.add("div1");
+//     rootElem.appendChild(div1)
+//   }
+
+
+//   div1.innerHTML =`<h2>${episodeList[i].name}-${episodeList[i].season} -${episodeList[i].number}</h2>
+//   <img src="${episodeList[i].image.medium}">
+//    <p>${episodeList[i].summary}</p>`;
+// }
+// window.onload = setup;
+
